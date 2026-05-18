33c951bc1399d8c0993db5613dded1c8021885b8
Elmira <elmira.sharapova@yandex.ru>
2026-05-18 14:57:56 +0500

    Пагинация с https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->trustProxies(at: '*');
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
