640d1b3961b4d60a2f28266486add7302022e410
Elmira <elmira.sharapova@yandex.ru>
2026-05-18 14:48:58 +0500

    Ссылка hhtps на продакшене

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // Включает HTTPS, если сайт работает на удаленном сервере (в production)
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
+
 }
